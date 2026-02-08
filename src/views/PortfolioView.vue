88f877cc25978326f41f98c241d740a13e15bf24
andremarthinus.l <2311035@students.universitasmulia.ac.id>
2026-02-08 15:28:10 +0800

    update

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,88 +6,173 @@ export default {
       items: [
         {
           id: 1,
-          name: 'Portal Habbo Agency',
-          imageUrl: 'portal',
-            status: 'This portal is designed to help users record all activities carried out in the agency such as promotions, trainings and others.',
-          tech: 'Laravel 11, Bootstrap 5',
+          name: "Portal Habbo Agency",
+          imageUrl: "portal",
+          status:
+            "This portal is designed to help users record all activities carried out in the agency such as promotions, trainings and others.",
+          tech: "Laravel 11, Bootstrap 5",
           github: null,
-          demo: null
-        }, 
+          demo: null,
+        },
         {
           id: 2,
-          name: 'IAF Portfolio Website',
-          imageUrl: 'iaf',
-          status: 'A custom portfolio website for the International Army Force, built to showcase activities, ranks, and resources for the Habbo agency roleplay community.',
-          tech: 'PHP, HTML, Bootstrap, JavaScript',
+          name: "IAF Portfolio Website",
+          imageUrl: "iaf",
+          status:
+            "A custom portfolio website for the International Army Force, built to showcase activities, ranks, and resources for the Habbo agency roleplay community.",
+          tech: "PHP, HTML, Bootstrap, JavaScript",
           github: null,
-          demo: 'https://habboiaf.com'
-        }
-      ]
+          demo: "https://habboiaf.com",
+        },
+        {
+          id: 3,
+          name: "Nimex Komix",
+          imageUrl: "nimex",
+          status:
+            "A streaming anime and manga reading website that fetches data from external sources like Otakudesu and Komikstation.",
+          tech: "Next.js",
+          github: null,
+          demo: "https://nimex-komix.vercel.app",
+        },
+      ],
     };
-  }
-}
+  },
+};
 </script>
-<template>  
-<div class="px-5 py-5 md:px-12 md:py-10 text-left mx-3" :class="$root.isLightMode ? 'text-gray-700' : 'text-blue-50'">
+<template>
+  <div
+    class="px-5 py-5 md:px-12 md:py-10 text-left mx-3"
+    :class="$root.isLightMode ? 'text-gray-700' : 'text-blue-50'"
+  >
     <article data-page="about">
-      <header>          <div
+      <header>
+        <div
           class="text-2xl font-bold mb-10 fadein-bot title-section flex items-center justify-center flex-col"
-          :class="$root.isLightMode ? 'text-gray-800' : 'text-white'">
-          <div class="h-[1px] w-10 bg-blue-500 md:w-20 aos-init aos-animate"></div>          
+          :class="$root.isLightMode ? 'text-gray-800' : 'text-white'"
+        >
+          <div
+            class="h-[1px] w-10 bg-blue-500 md:w-20 aos-init aos-animate"
+          ></div>
           <h4>Past Project Experience</h4>
-          <h4 class="text-base font-normal text-transparent bg-clip-text"
-            :class="$root.isLightMode ? 'bg-gradient-to-r from-gray-700 to-blue-500' : 'bg-gradient-to-r from-slate-100 to-blue-500'">
-            Explore the projects I've worked on so far</h4>
+          <h4
+            class="text-base font-normal text-transparent bg-clip-text"
+            :class="
+              $root.isLightMode
+                ? 'bg-gradient-to-r from-gray-700 to-blue-500'
+                : 'bg-gradient-to-r from-slate-100 to-blue-500'
+            "
+          >
+            Explore the projects I've worked on so far
+          </h4>
         </div>
       </header>
       <section>
         <div>
-          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">            <div v-for="item in items" :key="item.id">
+          <div
+            class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in"
+          >
+            <div v-for="item in items" :key="item.id">
               <div
                 class="item-card flex flex-col items-center gap-2 rounded-xl md:gap-3 px-5 py-5 lg:px-5"
-                :class="$root.isLightMode ? 'bg-white hover:bg-gray-100 border border-gray-200 text-gray-800' : 'bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] text-blue-50'">
-                <div class="flex items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
-                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded-xl"
-                    :src="'/img/portfolio-' + item.imageUrl + '.png'">
+                :class="
+                  $root.isLightMode
+                    ? 'bg-white hover:bg-gray-100 border border-gray-200 text-gray-800'
+                    : 'bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] text-blue-50'
+                "
+              >
+                <div
+                  class="flex items-center justify-center p-0 h-full w-full lg:p-0 zoom-in"
+                >
+                  <img
+                    alt="HTML"
+                    loading="lazy"
+                    decoding="async"
+                    data-nimg="1"
+                    class="drop-shadow-xl rounded-xl"
+                    :src="'/img/portfolio-' + item.imageUrl + '.png'"
+                  />
                 </div>
-                <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
-                  <div class="title-text font-medium text-secondary">{{ item.name }}
+                <div
+                  class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg"
+                >
+                  <div class="title-text font-medium text-secondary">
+                    {{ item.name }}
                   </div>
-                  <div class="w-full text-left text-[10px] md:text-xs lg:text-sm"
-                    :class="$root.isLightMode ? 'text-gray-600' : 'text-[#c1c1c1]'">
-                    {{ item.status }}</div><div class="w-full mt-4 text-normal text-sm text-left text-blue-300">
+                  <div
+                    class="w-full text-left text-[10px] md:text-xs lg:text-sm"
+                    :class="
+                      $root.isLightMode ? 'text-gray-600' : 'text-[#c1c1c1]'
+                    "
+                  >
+                    {{ item.status }}
+                  </div>
+                  <div
+                    class="w-full mt-4 text-normal text-sm text-left text-blue-300"
+                  >
                     {{ item.tech }}
                   </div>
                   <div class="w-full flex justify-end">
-                    <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'"
-                        :href="item.github" target="_blank" rel="noreferrer"
-                        title="View github repository" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <div
+                      class="flex cursor-pointer items-end gap-2 text-primary"
+                    >
+                      <a
+                        v-if="item.github !== 'null'"
+                        :href="item.github"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="View github repository"
+                        class="transition-all hover:text-accent"
+                      >
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          height="16"
+                          width="16"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
                           <path
-                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-                          </path>
-                        </svg></a>
-                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
-                        title="View finished project" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                          ></path></svg
+                      ></a>
+                      <a
+                        v-if="item.demo !== 'null'"
+                        :href="item.demo"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="View finished project"
+                        class="transition-all hover:text-accent"
+                      >
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          stroke-width="2"
+                          viewBox="0 0 24 24"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          height="18"
+                          width="18"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                          ></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg></a></div>
+                          <line x1="10" y1="14" x2="21" y2="3"></line></svg
+                      ></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </article>
-</div>
+      </section>
+    </article>
+  </div>
 </template>
 
 <style>
@@ -95,7 +180,7 @@ export default {
   transition: transform 0.3s ease;
   transform: translateY(-8px);
 }
-svg:hover{
+svg:hover {
   stroke: #3b82f6;
 }
 @keyframes fadeZoomIn {
